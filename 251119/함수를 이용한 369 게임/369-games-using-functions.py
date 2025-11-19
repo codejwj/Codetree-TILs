27a81cef85b634b28e3f1c68534b3bbbf70f60e0
codejwj <jsin1336@gmail.com>
2025-11-19 11:41:46 +0900

    Title: 함수를 이용한 369 게임, Time: 94ms, Memory: 18MB, Status: Passed - Codetree

--- 251119/함수를 이용한 369 게임/369-games-using-functions.py
+++ 251119/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,19 +1,18 @@
-A, B = map(int, input().split())
+A, B = tuple(map(int, input().split()))
 
+#3, 6, 9 숫자가 하나라도 포함
 def contains_369(x):
-    for digit in str(x):
-        if digit in ['3', '6', '9']:
+    #계속 10으로 나눠 일의 자리를 조사
+    while x > 0:
+        if x % 10 == 3 or x % 10 == 6 or x % 10 == 9:
             return True
-    return False
+        x //= 10
     
+    return False
+
+#3, 6, 9를 포함하거나 3의 배수인지 판단
 def is_369_number(x):
-    if x % 3 == 0:
-        return True
-    
-    if contains_369(x):
-        return True
-    
-    return False
+    return contains_369(x) or (x % 3 == 0)
 
 cnt = 0
 for i in range(A, B + 1):
