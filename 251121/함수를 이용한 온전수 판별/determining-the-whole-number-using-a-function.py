7f0d2d9419fcb464412908af18f1a50156d09b98
codejwj <jsin1336@gmail.com>
2025-11-21 16:14:44 +0900

    Title: 함수를 이용한 온전수 판별, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251121/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,17 @@
+A, B = tuple(map(int, input().split()))
+
+def is_onjeonsu(x):
+    if x % 2 == 0:
+        return False
+    if x % 10 == 5:
+        return False
+    if x % 3 == 0 and x % 9 != 0:
+        return False
+    return True
+
+cnt = 0
+for i in range(A, B + 1):
+    if is_onjeonsu(i):
+        cnt += 1
+
+print(cnt)
