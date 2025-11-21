8730481a1b27a9005e494c98f1bec872bd1bdb40
codejwj <jsin1336@gmail.com>
2025-11-21 16:08:51 +0900

    Title: 사칙연산 함수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251121/사칙연산 함수/quadratic-operations-function.py
+++ 251121/사칙연산 함수/quadratic-operations-function.py
@@ -2,25 +2,25 @@ a, o, c = input().split()
 a = int(a)
 c = int(c)
 
-def add(x, y):
-    return(f"{x} + {y} = {x + y}")
+def plus(x, y):
+    return x + y
 
-def sub(x, y):
-    return(f"{x} - {y} = {x - y}")
+def minus(x, y):
+    return x - y
 
-def multi(x, y):
-    return(f"{x} * {y} = {x * y}")
+def times(x, y):
+    return x * y
 
-def div(x, y):
-    return(f"{x} / {y} = {x // y}")
+def divide(x, y):
+    return x // y
 
 if o == '+':
-    print(add(a, c))
+    print(a, o, c, "=", plus(a, c))
 elif o == '-':
-    print(sub(a, c))
+    print(a, o, c, "=", minus(a, c))
 elif o == '*':
-    print(multi(a, c))
+    print(a, o, c, "=", times(a, c))
 elif o == '/':
-    print(div(a, c))
+    print(a, o, c, "=", divide(a, c))
 else:
     print("False")
