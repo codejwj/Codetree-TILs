b1eb4a02cb5cd36cf576640af40b6a59c8cd03e0
codejwj <jsin1336@gmail.com>
2025-11-26 11:28:44 +0900

    Title: 숫자 차례로 출력하기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 251126/숫자 차례로 출력하기/to-output-numerically.py
+++ 251126/숫자 차례로 출력하기/to-output-numerically.py
@@ -1,19 +1,19 @@
 N = int(input())
 
-def f(n):
+def print_number1(n):
     if n == 0:
         return
 
-    f(n - 1)
+    print_number1(n - 1)
     print(n, end = " ")
 
-def g(n):
+def print_number2(n):
     if n == 0:
         return 
     
     print(n, end = " ")
-    g(n - 1)
+    print_number2(n - 1)
 
-f(N)
+print_number1(N)
 print()
-g(N)
+print_number2(N)
