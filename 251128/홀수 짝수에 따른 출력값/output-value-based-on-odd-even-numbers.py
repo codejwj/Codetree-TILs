de193e7d4454ec9cb73630436b08632eed70d52d
codejwj <jsin1336@gmail.com>
2025-11-28 16:56:52 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 251128/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ 251128/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -1,15 +1,11 @@
 N = int(input())
 
 def f(n):
-    if n <= 1:
-        if n % 2 == 0:
-            return 0
-        else:
-            return 1
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
     
-    if n % 2 == 0:
-        return f(n - 2) + n
-    else:
-        return f(n - 2) + n
+    return f(n - 2) + n
     
 print(f(N))
