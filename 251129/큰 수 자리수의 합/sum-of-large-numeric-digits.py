123000a87abbb821de67fd13a28f87691b5a46b1
codejwj <jsin1336@gmail.com>
2025-11-29 21:59:45 +0900

    Title: 큰 수 자리수의 합, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251129/큰 수 자리수의 합/sum-of-large-numeric-digits.py
+++ 251129/큰 수 자리수의 합/sum-of-large-numeric-digits.py
@@ -1,5 +1,4 @@
-a, b, c = map(int, input().split())
-N = a * b * c
+a, b, c = tuple(map(int, input().split()))
 
 def f(n):
     if n < 10:
@@ -7,4 +6,5 @@ def f(n):
     
     return f(n // 10) + n % 10
 
+N = a * b * c
 print(f(N))
