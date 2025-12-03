768c5139df58607adc055e9dca82428c97788696
codejwj <jsin1336@gmail.com>
2025-12-03 23:53:13 +0900

    Title: 007, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251203/007/007.py
+++ 251203/007/007.py
@@ -1,12 +1,14 @@
-secret_code, meeting_point, time = input().split()
-time = int(time)
+secret_code, meeting_point, time = tuple(input().split())
 
-class Sceret:
+#클래스 선언
+class Spy:
     def __init__(self, secret_code, meeting_point, time):
         self.secret_code = secret_code
         self.meeting_point = meeting_point
         self.time = time
 
-print(f"secret code : {secret_code}")
-print(f"meeting point : {meeting_point}")
-print(f"time : {time}")
+s = Spy(secret_code, meeting_point, int(time))
+
+print(f"secret code : {s.secret_code}")
+print(f"meeting point : {s.meeting_point}")
+print(f"time : {s.time}")
