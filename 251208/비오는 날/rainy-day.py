49fddb29ec24dd316831f8968c763026bf664854
codejwj <jsin1336@gmail.com>
2025-12-08 18:25:17 +0900

    Title: 비오는 날, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251208/비오는 날/rainy-day.py
+++ 251208/비오는 날/rainy-day.py
@@ -8,10 +8,12 @@ n = int(input())
 arr = [tuple(input().split()) for _ in range(n)]
 rain = [Rain(date, day, weather) for date, day, weather in arr]
 
-idx = -1
+idx = None
 for i in range(n):
     if rain[i].weather == "Rain":
-        if rain[i].date < rain[idx].date:
+        if idx is None:
+            idx = i
+        elif rain[i].date < rain[idx].date:
             idx = i
 
 print(rain[idx].date, rain[idx].day, rain[idx].weather)
