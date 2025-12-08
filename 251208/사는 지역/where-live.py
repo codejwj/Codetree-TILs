af331b2e0948e793dcba38aa8d77cb33c52a2650
codejwj <jsin1336@gmail.com>
2025-12-08 17:41:01 +0900

    Title: 사는 지역, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251208/사는 지역/where-live.py
+++ 251208/사는 지역/where-live.py
@@ -1,21 +1,22 @@
-class Home:
+class Address:
     def __init__(self, name, addr, city):
         self.name = name
         self.addr = addr
         self.city = city
 
-homes = []
+people = []
 
 n = int(input())
 for _ in range(n):
     name, addr, city = tuple(input().split())
-    homes.append(Home(name, addr, city))
+    people.append(Address(name, addr, city))
 
+#사전순으로 이름이 가장 느린 사람 찾기
 idx = 0
 for i in range(n):
-    if homes[idx].name < homes[i].name:
+    if people[idx].name < people[i].name:
         idx = i
 
-print(f"name {homes[idx].name}")
-print(f"addr {homes[idx].addr}")
-print(f"city {homes[idx].city}")
+print(f"name {people[idx].name}")
+print(f"addr {people[idx].addr}")
+print(f"city {people[idx].city}")
