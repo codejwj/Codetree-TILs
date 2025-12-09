ec9ef3ed30a72f7e0203b014cbe768441b898ef7
codejwj <jsin1336@gmail.com>
2025-12-09 14:28:13 +0900

    Title: 비오는 날, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251209/비오는 날/rainy-day.py
@@ -0,0 +1,19 @@
+class Forecast:
+    def __init__(self, date, day, weather):
+        self.date = date
+        self.day = day
+        self.weather = weather
+
+n = int(input())
+arr = [tuple(input().split()) for _ in range(n)]
+rain = [Forecast(date, day, weather) for date, day, weather in arr]
+
+idx = None
+for i in range(n):
+    if rain[i].weather == "Rain":
+        if idx is None:
+            idx = i
+        elif rain[i].date < rain[idx].date:
+            idx = i
+
+print(rain[idx].date, rain[idx].day, rain[idx].weather)
