36db9c203c9dad12c7c37a81e74e166dfae735ee
codejwj <jsin1336@gmail.com>
2025-12-14 16:01:54 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251214/DateTime to DateTime/datetime-to-datetime.py
+++ 251214/DateTime to DateTime/datetime-to-datetime.py
@@ -2,19 +2,22 @@ A, B, C = map(int, input().split())
 day, hour, mins = 11, 11, 11
 elapsed_time = 0
 
-while True:
-    if day == A and hour == B and mins == C:
-        break
+if A < day or (A == day and B < hour) or (A == day and B == hour and C < mins):
+    print(-1)
+else:
+    while True:
+        if day == A and hour == B and mins == C:
+            break
 
-    elapsed_time += 1
-    mins += 1
+        elapsed_time += 1
+        mins += 1
 
-    if mins == 60:
-        hour += 1
-        mins = 0
+        if mins == 60:
+            hour += 1
+            mins = 0
 
-        if hour == 24:
-            day += 1
-            hour = 0
-
-print(elapsed_time)
+            if hour == 24:
+                day += 1
+                hour = 0
+    
+    print(elapsed_time)
