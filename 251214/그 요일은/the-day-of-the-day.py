dba84663399e80a6717db5e977ece6f6faae8649
codejwj <jsin1336@gmail.com>
2025-12-14 18:01:11 +0900

    Title: 그 요일은, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251214/그 요일은/the-day-of-the-day.py
+++ 251214/그 요일은/the-day-of-the-day.py
@@ -1,28 +1,26 @@
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
 
-num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-sum_days = [0]
+def num_of_days(m, d):
+    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    
+    for i in range(1, m):
+        total_days += days[i]
 
-for i in range(12):
-    sum_days.append(sum_days[i] + num_of_days[i + 1])
-
-day_1 = sum_days[m1 - 1] + d1
-day_2 = sum_days[m2 - 1] + d2
-N = day_2 - day_1
+    total_days += d
+    
+    return total_days    
 
 day = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-start_idx = (day_1 - 1) % 7
+diff = num_of_days(m2, d2) - num_of_days(m1, d1) 
+
 target_idx = day.index(A)
+cnt = diff // 7 
+remainder = diff % 7
 
-cnt = N // 7
-r = N % 7
-
-if r > 0:
-    for i in range(r):
-        if (start_idx + 1 + i) % 7 == target_idx:
-            cnt += 1
-            break
+if target_idx <= remainder:
+    cnt += 1
 
 print(cnt)
