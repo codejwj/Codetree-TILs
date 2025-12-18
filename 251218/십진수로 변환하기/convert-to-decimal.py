0042cfc53123455faf340e1d4a616472f83aa1f4
codejwj <jsin1336@gmail.com>
2025-12-18 15:23:38 +0900

    Title: 십진수로 변환하기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251218/십진수로 변환하기/convert-to-decimal.py
+++ 251218/십진수로 변환하기/convert-to-decimal.py
@@ -1,8 +1,7 @@
-binary = input()
-list(str(binary))
+binary = list(map(int, list(input())))
 num = 0
 
 for i in range(len(binary)):
-    num = num * 2 + int(binary[i])
+    num = num * 2 + binary[i]
 
 print(num)
