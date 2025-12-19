43bdf43cab83f5d3aee9dfeb2db8176823e7b67f
codejwj <jsin1336@gmail.com>
2025-12-19 16:29:18 +0900

    Title: 사각형의 총 넓이 2, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 251219/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 251219/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,23 +1,24 @@
 OFFSET = 100
 
 N = int(input())
-x1, y1, x2, y2 = [], [], [], []
-for _ in range(N):
-    a, b, c, d = map(int, input().split())
-    x1.append(a)
-    y1.append(b)
-    x2.append(c)
-    y2.append(d)
+rects = [
+    tuple(map(int, input().split()))
+    for _ in range(N)
+]
 
-plane = [[0] * 200 for _ in range(200)]
+checked = [[0] * 201 for _ in range(201)]
 
-for k in range(N):
-    for i in range(x1[k] + OFFSET, x2[k] + OFFSET):
-        for j in range(y1[k] + OFFSET, y2[k] + OFFSET):
-            plane[i][j] = 1
+for x1, y1, x2, y2 in rects:
+    x1, y1 = x1 + OFFSET, y1 + OFFSET
+    x2, y2 = x2 + OFFSET, y2 + OFFSET
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            checked[i][j] = 1
 
-total = 0
-for row in plane:
-    total += sum(row)
+area = 0
+for x in range(0, 201):
+    for y in range(0, 201):
+        if checked[x][y]:
+            area += 1
 
-print(total)
+print(area)
