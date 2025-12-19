3ea289c0cb717ffc8ccef6b7135719b30a0bd64d
codejwj <jsin1336@gmail.com>
2025-12-19 16:57:38 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 214ms, Memory: 48MB, Status: Passed - Codetree

--- 251219/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 251219/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -1,34 +1,28 @@
 OFFSET = 1000
 MAX_R = 2000
 
-A = tuple(map(int, input().split()))
-B = tuple(map(int, input().split()))
-M = tuple(map(int, input().split()))
+N = 3
+rects = [
+    tuple(map(int, input().split()))
+    for _ in range(N)
+]
 
 checked = [
     [0] * (MAX_R + 1)
     for _ in range(MAX_R + 1)
 ]
 
-for rect in [A, B]:
-    x1, y1, x2, y2 = rect
+for i, (x1, y1, x2, y2) in enumerate(rects, start = 1):
     x1, y1 = x1 + OFFSET, y1 + OFFSET
     x2, y2 = x2 + OFFSET, y2 + OFFSET
-    for i in range(x1, x2):
-        for j in range(y1, y2):
-            checked[i][j] = 1
-
-x1, y1, x2, y2 = M
-x1, y1 = x1 + OFFSET, y1 + OFFSET
-x2, y2 = x2 + OFFSET, y2 + OFFSET
-for i in range(x1, x2):
-    for j in range(y1, y2):
-        checked[i][j] = 0
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            checked[x][y] = i
 
 area = 0
 for x in range(0, MAX_R + 1):
     for y in range(0, MAX_R + 1):
-        if checked[x][y]:
+        if checked[x][y] == 1 or checked[x][y] == 2:
             area += 1
 
 print(area)
