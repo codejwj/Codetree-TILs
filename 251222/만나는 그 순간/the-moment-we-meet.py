53f756e89f2cc6040eb657445ac30047fdd71a2b
codejwj <jsin1336@gmail.com>
2025-12-22 15:54:11 +0900

    Title: 만나는 그 순간, Time: 126ms, Memory: 35MB, Status: Passed - Codetree

---
+++ 251222/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,49 @@
+CUR = 100000
+MAX_T = 1000000
+
+N, M = tuple(map(int, input().split()))
+
+A = [0] * (MAX_T + 1)
+B = [0] * (MAX_T + 1)
+
+time = 0
+cur = CUR
+for _ in range(N):
+    d1, t1 = tuple(input().split())
+    t1 = int(t1)
+    if d1 == 'L':
+        for _ in range(t1):
+            time += 1
+            cur -= 1
+            A[time] = cur
+    elif d1 == 'R':
+        for _ in range(t1):
+            time += 1
+            cur += 1
+            A[time] = cur
+
+time = 0
+cur = CUR
+for _ in range(M):
+    d2, t2 = tuple(input().split())
+    t2 = int(t2)
+    if d2 == 'L':
+        for _ in range(t2):
+            time += 1
+            cur -= 1
+            B[time] = cur
+    elif d2 == 'R':
+        for _ in range(t2):
+            time += 1
+            cur += 1
+            B[time] = cur
+
+total_time = time
+
+ans = -1
+for i in range(1, total_time + 1):
+    if A[i] == B[i]:
+        ans = i
+        break
+
+print(ans)
