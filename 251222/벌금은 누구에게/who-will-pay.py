b90b543059d9f105811d3d5ec62ecece98d13259
codejwj <jsin1336@gmail.com>
2025-12-22 16:31:10 +0900

    Title: 벌금은 누구에게, Time: 63ms, Memory: 16MB, Status: Passed - Codetree

--- 251222/벌금은 누구에게/who-will-pay.py
+++ 251222/벌금은 누구에게/who-will-pay.py
@@ -3,8 +3,11 @@ student = [int(input()) for _ in range(M)]
 
 checked = [0] * (N + 1)
 
+ans = -1
 for i in range(M):
     checked[student[i]] += 1
     if checked[student[i]] >= K:
-        print(student[i])
+        ans = student[i]
         break
+
+print(ans)
