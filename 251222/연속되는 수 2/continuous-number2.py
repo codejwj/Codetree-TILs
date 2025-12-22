1417dbab0fb795cb158a62252084c38d7737c6d8
codejwj <jsin1336@gmail.com>
2025-12-22 14:59:10 +0900

    Title: 연속되는 수 2, Time: 64ms, Memory: 16MB, Status: Passed - Codetree

--- 251222/연속되는 수 2/continuous-number2.py
+++ 251222/연속되는 수 2/continuous-number2.py
@@ -1,10 +1,10 @@
-n = int(input())
-arr = [int(input()) for _ in range(n)]
+N = int(input())
+arr = [int(input()) for _ in range(N)]
 
 cnt = 0
 max_cnt = 0
 
-for i in range(n):
+for i in range(N):
     if i == 0 or arr[i] == arr[i - 1]:
         cnt += 1
     else:
