48989e49a5b679eabc3f4502176fa4a2eaebe9dd
codejwj <jsin1336@gmail.com>
2025-12-24 21:20:22 +0900

    Title: 1이 3개 이상 있는 위치, Time: 81ms, Memory: 19MB, Status: Passed - Codetree

--- 251224/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 251224/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,21 +1,28 @@
 N = int(input())
-grid = [list(map(int, input().split())) for _ in range(N)]
+grid = [
+    list(map(int, input().split())) 
+    for _ in range(N)
+]
+
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
 
 def in_range(x, y):
     return 0 <= x and x < N and 0 <= y and y < N
 
-dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+def adjacent_cnt(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and grid[nx][ny] == 1:
+            cnt += 1
+    
+    return cnt
 
 ans = 0
 for i in range(N):
     for j in range(N):
-        x, y = i, j
-        cnt = 0
-        for dx, dy in zip(dxs, dys):   
-            nx, ny = x + dx, y + dy
-            if in_range(nx, ny) and grid[nx][ny] == 1:
-                cnt += 1
-        if cnt >= 3:
+        if adjacent_cnt(i, j) >= 3:
             ans += 1
 
 print(ans)
