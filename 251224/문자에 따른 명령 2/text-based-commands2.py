52058523c7cbc855d9d928ea5fb9f9f3504ec5cb
codejwj <jsin1336@gmail.com>
2025-12-24 18:57:36 +0900

    Title: 문자에 따른 명령 2, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 251224/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,17 @@
+D = input()
+
+x, y = 0, 0
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+
+dir_num = 3
+for elem in D:
+    if elem == 'L':
+        dir_num = (dir_num - 1 + 4) % 4
+    elif elem == 'R':
+        dir_num = (dir_num + 1) % 4
+    
+    if elem == 'F':
+        x += dx[dir_num]
+        y += dy[dir_num]
+
+print(x, y)
