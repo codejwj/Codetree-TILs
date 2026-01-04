b2c31e294118d446ac6b22f3d0d2a096911feae8
codejwj <jsin1336@gmail.com>
2026-01-04 12:50:23 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260104/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 260104/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -13,17 +13,17 @@ dys = [1, 0, -1, 0]
 x, y = 0, 0
 dir_num = 0
 
-arr[x][y] = 1
+arr[x][y] = 'A'
 
-for i in range(2, N * M + 1):
+for i in range(1, N * M):
     nx, ny = x + dxs[dir_num], y + dys[dir_num]
     if not in_range(nx, ny) or arr[nx][ny] != 0:
         dir_num = (dir_num + 1) % 4
 
     x, y = x + dxs[dir_num], y + dys[dir_num]
-    arr[x][y] = i
+    arr[x][y] = chr((i % 26) + ord('A'))
 
 for i in range(N):
     for j in range(M):
-        print(chr(((arr[i][j] - 1) % 26) + 65), end = ' ')
+        print(arr[i][j], end = ' ')
     print()
