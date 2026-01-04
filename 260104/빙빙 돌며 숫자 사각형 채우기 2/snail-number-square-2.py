81677e3c24b3b344858a10b4b0a42502f0449e6e
codejwj <jsin1336@gmail.com>
2026-01-04 12:04:52 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260104/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,29 @@
+N, M = tuple(map(int, input().split()))
+arr = [
+    [0] * M
+    for _ in range(N)
+]
+
+def in_range(x, y):
+    return 0 <= x and x < N and 0 <= y and y < M
+
+dxs = [1, 0, -1, 0]
+dys = [0, 1, 0, -1]
+
+x, y = 0, 0
+dir_num = 0
+
+arr[x][y] = 1
+
+for i in range(2, N * M + 1):
+    nx, ny = x + dxs[dir_num], y + dys[dir_num]
+    if not in_range(nx, ny) or arr[nx][ny] != 0:
+        dir_num = (dir_num + 1) % 4
+    
+    x, y = x + dxs[dir_num], y + dys[dir_num]
+    arr[x][y] = i
+
+for i in range(N):
+    for j in range(M):
+        print(arr[i][j], end = ' ')
+    print()
