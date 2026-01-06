6da31306103ac54adf91a09c74bc0b62d1d18e4f
codejwj <jsin1336@gmail.com>
2026-01-06 12:46:29 +0900

    Title: 최고의 13위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260106/최고의 13위치/best-place-of-13.py
+++ 260106/최고의 13위치/best-place-of-13.py
@@ -7,6 +7,7 @@ grid = [
 max_cnt = 0
 for i in range(N):
     for j in range(N - 2):
-        max_cnt = max(max_cnt, grid[i][j] + grid[i][j + 1] + grid[i][j + 2])
-
+        cnt = grid[i][j] + grid[i][j + 1] + grid[i][j + 2]
+        max_cnt = max(max_cnt, cnt)
+        
 print(max_cnt)
