e343985041cafab9ba5284f5d3d283f68e5b1c63
codejwj <jsin1336@gmail.com>
2026-01-07 15:48:31 +0900

    Title: 인접하지 않은 2개의 수, Time: 50ms, Memory: 17MB, Status: Passed - Codetree

--- 260107/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
+++ 260107/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
@@ -6,8 +6,7 @@ max_sum = 0
 for i in range(N):
     sum_val = 0
     for j in range(i + 2, N):
-        sum_val = num[i] + num[j]
-    
-    max_sum = max(max_sum, sum_val) 
+        sum_val = num[i] + num[j]   
+        max_sum = max(max_sum, sum_val) 
 
 print(max_sum)
