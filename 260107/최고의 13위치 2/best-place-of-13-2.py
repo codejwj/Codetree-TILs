cd0bd6bf2bd846254a01fe6399cf9e4d5f245e64
codejwj <jsin1336@gmail.com>
2026-01-07 17:45:13 +0900

    Title: 최고의 13위치 2, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 260107/최고의 13위치 2/best-place-of-13-2.py
+++ 260107/최고의 13위치 2/best-place-of-13-2.py
@@ -7,10 +7,16 @@ arr = [
 max_cnt = 0
 for i in range(N):
     for j in range(N - 2):
-        for k in range(i + 1, N):
+        cnt1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+        for k in range(N):
             for l in range(N - 2):
-                cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[k][l] + arr[k][l + 1] + arr[k][l + 2]
+                if i == k and abs(j - l) >= 3:
+                    cnt2 = arr[k][l] + arr[k][l + 1] + arr[k][l + 2]
+
+                    max_cnt = max(max_cnt, cnt1 + cnt2)
+                elif i != k:
+                    cnt2 = arr[k][l] + arr[k][l + 1] + arr[k][l + 2]
                 
-                max_cnt = max(max_cnt, cnt)
+                    max_cnt = max(max_cnt, cnt1 + cnt2)
 
 print(max_cnt)
