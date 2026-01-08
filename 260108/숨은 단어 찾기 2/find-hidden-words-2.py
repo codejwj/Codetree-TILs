9138cbf20a4fa8f4fc091fb2e88e8606a5885982
codejwj <jsin1336@gmail.com>
2026-01-08 18:18:47 +0900

    Title: 숨은 단어 찾기 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260108/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 260108/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -1,22 +1,30 @@
 N, M = tuple(map(int, input().split()))
-arr = [input() for _ in range(N)]
+arr = [
+    input() 
+    for _ in range(N)
+]
 
 def in_range(x, y):
     return 0 <= x and x < N and 0 <= y and y < M
 
+#가로, 세로, 대각선으로 인접한 8칸
 dxs = [0, 0, 1, -1, 1, 1, -1, -1]
 dys = [1, -1, 0 ,0, 1, -1, 1, -1]
 
 cnt = 0
+#모든 좌표에서 다 확인
 for r in range(N):
     for c in range(M):
-        if arr[r][c] == 'L':
-            for i in range(8):
-                nx2, ny2 = r + dxs[i], c + dys[i]
-                nx3, ny3 = r + dxs[i] * 2, c + dys[i] * 2
+        #시작점 'L' 찾기
+        if arr[r][c] != 'L':
+            continue
 
+        for dx, dy in zip(dxs, dys):
+            nx1, ny1 = r + dx, c + dy
+            nx2, ny2 = r + dx * 2, c + dy * 2
+
+            if in_range(nx1, ny1) and arr[nx1][ny1] == 'E':
                 if in_range(nx2, ny2) and arr[nx2][ny2] == 'E':
-                    if in_range(nx3, ny3) and arr[nx3][ny3] == 'E':
-                        cnt += 1 
+                    cnt += 1 
 
 print(cnt)
