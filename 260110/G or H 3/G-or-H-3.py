76abef2414ba648ec30d558720f31479aba274af
codejwj <jsin1336@gmail.com>
2026-01-10 16:03:25 +0900

    Title: G or H 3, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 260110/G or H 3/G-or-H-3.py
+++ 260110/G or H 3/G-or-H-3.py
@@ -1,24 +1,22 @@
-import sys
-
-INT_MIN = -sys.maxsize
+MAX_NUM = 10000
 
 N, K = tuple(map(int, input().split()))
-arr = [0] * 20001
+arr = [0] * (MAX_NUM + 1)
 
 for _ in range(N):
-    pos, char = input().split()
-    pos = int(pos)
-    arr[pos] = char
+    x, c = input().split()
+    x = int(x)
+    arr[x] = c
 
-max_score = INT_MIN
-for i in range(1, 10001):
-    score = 0
+max_sum = 0
+for i in range(MAX_NUM - K + 1):
+    sum_val = 0
     for j in range(i, i + K + 1):
         if arr[j] == 'G':
-            score += 1
+            sum_val += 1
         elif arr[j] == 'H':
-            score += 2
+            sum_val += 2
 
-    max_score = max(max_score, score)
+    max_sum = max(max_sum, sum_val)
 
-print(max_score)
+print(max_sum)
