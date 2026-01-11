a7d630b53befaf91ff11cd26f31ba4bf17e083e9
codejwj <jsin1336@gmail.com>
2026-01-11 18:20:33 +0900

    Title: 아름다운 수열 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260111/아름다운 수열 2/beautiful-sequence-2.py
+++ 260111/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,13 +2,9 @@ N, M = tuple(map(int, input().split()))
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-B.sort()
-
 cnt = 0
 for i in range(N - M + 1):
-    C = A[i : i + M]
-    C.sort()
-    if C == B:
+    if sorted(A[i : i + M]) == sorted(B):
         cnt += 1
 
 print(cnt)
