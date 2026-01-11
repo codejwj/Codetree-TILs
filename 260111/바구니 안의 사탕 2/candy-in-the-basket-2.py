6d7b969ab6e97ed637861828fc7a65023b79ce18
codejwj <jsin1336@gmail.com>
2026-01-11 22:09:23 +0900

    Title: 바구니 안의 사탕 2, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 260111/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 260111/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,4 +1,4 @@
-MAX_NUM = 200
+MAX_NUM = 100
 
 N, K = tuple(map(int, input().split()))
 arr = [0] * (MAX_NUM + 1)
@@ -8,12 +8,11 @@ for _ in range(N):
     arr[x] += candy
 
 max_cnt = 0
-for c in range(MAX_NUM - K + 1):
+for c in range(MAX_NUM):
     cnt = 0
-    start = max(0, c - K)
-    end = min(MAX_NUM, c+ K)
-    for i in range(start, end + 1):
-        cnt += arr[i]
+    for i in range(c - K, c + K + 1):
+        if i >= 0 and i <= MAX_NUM:
+            cnt += arr[i]
     
     max_cnt = max(max_cnt, cnt)
 
