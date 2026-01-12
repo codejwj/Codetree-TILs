7558621a653558de0c9be18f46156c9635c5caaa
codejwj <jsin1336@gmail.com>
2026-01-12 11:59:19 +0900

    Title: 개발팀의 능력, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260112/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 260112/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -6,6 +6,9 @@ N = 5
 arr = list(map(int, input().split()))
 
 def diff(x, y, z):
+    if arr[x] == arr[y] == arr[z]:
+        return -1
+        
     sum1 = arr[x] + arr[y]
     sum2 = arr[z]
     sum3 = sum(arr) - sum1 - sum2
