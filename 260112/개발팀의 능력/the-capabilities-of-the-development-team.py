95f68b76ae9bf4e60bd5353501aa1e2beeaa5cd5
codejwj <jsin1336@gmail.com>
2026-01-12 12:21:38 +0900

    Title: 개발팀의 능력, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260112/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 260112/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -10,16 +10,19 @@ def diff(x, y, z):
     sum2 = arr[y] + arr[z]
     sum3 = sum(arr) - sum1 - sum2
 
+    #하나라도 합이 같은 팀이 있으면 불가능한 경우
     if sum1 == sum2 or sum2 == sum3 or sum3 == sum1:
         return INT_MAX
 
     return abs(max(sum1, sum2, sum3) - min(sum1, sum2, sum3))
 
 min_diff = INT_MAX
+#첫 번째 팀원 i
 for i in range(N):
+    #두 번째 팀원 j, k
     for j in range(N):
         for k in range(j + 1, N):
-            if i == k or i == j:
+            if i == j or i == k:
                 continue
 
             min_diff = min(min_diff, diff(i, j, k))
