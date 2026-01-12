00aceeac9932e7d215d4a4ae9093c6f7bd907512
codejwj <jsin1336@gmail.com>
2026-01-12 12:10:40 +0900

    Title: 개발팀의 능력, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260112/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 260112/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -6,18 +6,25 @@ N = 5
 arr = list(map(int, input().split()))
 
 def diff(x, y, z):
-    if arr[x] == arr[y] == arr[z]:
-        return -1
-        
-    sum1 = arr[x] + arr[y]
-    sum2 = arr[z]
+    sum1 = arr[x]
+    sum2 = arr[y] + arr[z]
     sum3 = sum(arr) - sum1 - sum2
+
+    if sum1 == sum2 or sum2 == sum3 or sum3 == sum1:
+        return INT_MAX
+
     return abs(max(sum1, sum2, sum3) - min(sum1, sum2, sum3))
 
 min_diff = INT_MAX
 for i in range(N):
-    for j in range(i + 1, N):
+    for j in range(N):
         for k in range(j + 1, N):
+            if i == k or i == j:
+                continue
+
             min_diff = min(min_diff, diff(i, j, k))
 
-print(min_diff)
+if min_diff == INT_MAX:
+    print(-1)
+else:
+    print(min_diff)
