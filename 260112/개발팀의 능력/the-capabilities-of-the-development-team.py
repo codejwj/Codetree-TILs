9cdaec1e4b8a3c76d7309ef44e0d5fd22225f9a1
codejwj <jsin1336@gmail.com>
2026-01-12 11:53:52 +0900

    Title: 개발팀의 능력, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260112/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,20 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+N = 5
+arr = list(map(int, input().split()))
+
+def diff(x, y, z):
+    sum1 = arr[x] + arr[y]
+    sum2 = arr[z]
+    sum3 = sum(arr) - sum1 - sum2
+    return abs(max(sum1, sum2, sum3) - min(sum1, sum2, sum3))
+
+min_diff = INT_MAX
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            min_diff = min(min_diff, diff(i, j, k))
+
+print(min_diff)
