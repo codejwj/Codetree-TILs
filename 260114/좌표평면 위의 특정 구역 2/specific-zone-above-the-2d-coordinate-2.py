05e6ff4a144a0a4227ecd61fd097acfd6587b0bc
codejwj <jsin1336@gmail.com>
2026-01-14 15:20:59 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260114/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 260114/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -1,24 +1,26 @@
 import sys
 
 INT_MAX = sys.maxsize
-INT_MIN = -sys.maxsize
 
 N = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(N)]
+arr = [
+    tuple(map(int, input().split())) 
+    for _ in range(N)
+]
 
 min_size = INT_MAX
 for i in range(N):
-    x1, x2 = INT_MAX, INT_MIN
-    y1, y2 = INT_MAX, INT_MIN
-    for j in range(N):
+    x1, x2 = INT_MAX, 1
+    y1, y2 = INT_MAX, 1
+    for j, (x, y) in enumerate(arr):
+        #i번째 점은 제외
         if j == i:
             continue
         
-        x, y = arr[j]
-        x1 = min(x1, arr[j][0])
-        y1 = min(y1, arr[j][1])
-        x2 = max(x2, arr[j][0])
-        y2 = max(y2, arr[j][1])
+        x1 = min(x1, x)
+        y1 = min(y1, y)
+        x2 = max(x2, x)
+        y2 = max(y2, y)
 
     size = (x2 - x1) * (y2 - y1)
     min_size = min(min_size, size)
