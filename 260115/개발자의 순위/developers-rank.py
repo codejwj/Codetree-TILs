b2ddae3387cd77b2780da0123864b12f23a4520a
codejwj <jsin1336@gmail.com>
2026-01-15 21:37:08 +0900

    Title: 개발자의 순위, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260115/개발자의 순위/developers-rank.py
+++ 260115/개발자의 순위/developers-rank.py
@@ -11,6 +11,8 @@ for a in range(1, N + 1):
             continue
         
         is_always_better = True
+        #K번의 모든 경기에 대해서 두 개발자의 위치를 찾음
+        #하나라도 a번 개발자가 b번 개발자보다 뒤에 있으면 False로 변경
         for i in range(K):
             rank_a = arr[i].index(a)
             rank_b = arr[i].index(b)
