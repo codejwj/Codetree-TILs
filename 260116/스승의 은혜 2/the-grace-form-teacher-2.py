1765b5f958bcc113bb836f1544022d27d8f7723a
codejwj <jsin1336@gmail.com>
2026-01-16 16:34:13 +0900

    Title: 스승의 은혜 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260116/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,28 @@
+N, B = tuple(map(int, input().split()))
+P = [
+    int(input()) 
+    for _ in range(N)
+]
+
+P.sort()
+
+max_cnt = 0
+for i in range(N):
+    temp_B = B - (P[i] // 2)
+    if temp_B < 0:
+        continue
+
+    cnt = 1
+    for j in range(N):
+        if j == i:
+            continue
+
+        if temp_B > P[j]:
+            temp_B -= P[j]
+            cnt += 1
+        else:
+            break
+
+    max_cnt = max(max_cnt, cnt)
+
+print(max_cnt) 
