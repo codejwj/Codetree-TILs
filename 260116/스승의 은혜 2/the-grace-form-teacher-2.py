f56d95b37ae8c817b26166711e79bd6ebc6f4c88
codejwj <jsin1336@gmail.com>
2026-01-16 16:38:24 +0900

    Title: 스승의 은혜 2, Time: 62ms, Memory: 16MB, Status: Passed - Codetree

--- 260116/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 260116/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -6,23 +6,23 @@ P = [
 
 P.sort()
 
-max_cnt = 0
+ans = 0
 for i in range(N):
     temp_B = B - (P[i] // 2)
     if temp_B < 0:
         continue
 
-    cnt = 1
+    student = 1
     for j in range(N):
         if j == i:
             continue
 
         if temp_B >= P[j]:
             temp_B -= P[j]
-            cnt += 1
+            student += 1
         else:
             break
 
-    max_cnt = max(max_cnt, cnt)
+    ans = max(ans, student)
 
-print(max_cnt) 
+print(ans) 
