76042d7b214cbabb4005ad1cc5f021fc81bfa7c5
codejwj <jsin1336@gmail.com>
2026-01-17 11:32:30 +0900

    Title: 데이터센터의 온도 조정 2, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 260117/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
+++ 260117/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -1,24 +1,26 @@
+MAX_T = 1000
+
 N, C, G, H = tuple(map(int, input().split()))
 ranges = [
     tuple(map(int, input().split())) 
     for _ in range(N)
 ]
 
-def datasenter(current_temp, T1, T2):
-    if current_temp < T1:
+def performance(t, t1, t2):
+    if t < t1:
         return C
-    elif current_temp <= T2:
+    elif t <= t2:
         return G
     else:
         return H
 
 ans = 0
-for T in range(1000):
+for T in range(MAX_T + 1):
     sum_val = 0
     for i in range(N):
         T1 = ranges[i][0]
         T2 = ranges[i][1]
-        sum_val += datasenter(T, T1, T2)
+        sum_val += performance(T, T1, T2)
 
     ans = max(ans, sum_val)
 
