5f952d8e2f67dbfbc1f0adb48b63fc7aca3d0d67
codejwj <jsin1336@gmail.com>
2026-01-17 10:58:01 +0900

    Title: 숫자들의 합 중 최대, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260117/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 260117/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -1,12 +1,16 @@
 X, Y = tuple(map(int, input().split()))
 
-ans = 0
-for num in range(X, Y + 1):
-    str_num = str(num)
-    char_num = list(str_num)
-    int_num = list(map(int, char_num))
-    sum_val = sum(int_num)
+#각 자리의 숫자의 합을 구하여 반환
+def digit_sum(n):
+    if n < 10:
+        return n
+    #두 자리 이상의 숫자라면, 맨 끝자리를 제외한 숫자들의 합을
+    #재귀적으로 호출한 뒤, 그 결과가 마지막 자릿수를 더해 반환
+    else:
+        return digit_sum(n // 10) + (n % 10)
 
-    ans = max(ans, sum_val)
+ans = 0
+for i in range(X, Y + 1):
+    ans = max(ans, digit_sum(i))
     
 print(ans)
