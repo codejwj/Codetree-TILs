88c1fcbf8bc0f2dfb18c809005eab0e1e7fe23fc
codejwj <jsin1336@gmail.com>
2026-01-17 12:19:29 +0900

    Title: 팰린드롬 수 찾기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260117/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -0,0 +1,17 @@
+X, Y = tuple(map(int, input().split()))
+
+def palindrome_num(n):
+    l = len(str(n))
+    num = list(str(n))
+    for i in range(l // 2):
+        if num[i] != num[l - 1 - i]:
+            break
+    
+    return True
+
+cnt = 0
+for i in range(X, Y + 1):
+    if palindrome_num(i):
+        cnt += 1
+
+print(cnt)
