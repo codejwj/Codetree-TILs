435e5a73850eb4564b1bf1371f0f36bc4d0d0ac7
codejwj <jsin1336@gmail.com>
2026-01-17 12:24:20 +0900

    Title: 팰린드롬 수 찾기, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 260117/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 260117/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -1,17 +1,10 @@
 X, Y = tuple(map(int, input().split()))
 
-def palindrome_num(n):
-    l = len(str(n))
-    num = list(str(n))
-    for i in range(l // 2):
-        if num[i] != num[l - 1 - i]:
-            return False
-    
-    return True
-
 cnt = 0
 for i in range(X, Y + 1):
-    if palindrome_num(i):
+    str_i = str(i)
+    #펠린드롬 수는 거꾸로 읽어도 똑같은 수
+    if str_i == str_i[::-1]:
         cnt += 1
 
 print(cnt)
