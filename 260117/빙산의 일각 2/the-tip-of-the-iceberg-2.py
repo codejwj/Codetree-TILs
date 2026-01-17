5d8ea989d0b05439e9480592d89bb4f358258f76
codejwj <jsin1336@gmail.com>
2026-01-17 12:39:32 +0900

    Title: 빙산의 일각 2, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260117/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -0,0 +1,23 @@
+MAX_NUM = 1000
+
+N = int(input())
+H = [
+    int(input()) 
+    for _ in range(N)
+]
+
+max_cnt = 0
+for i in range(1, MAX_NUM + 1):
+    cnt = 0
+    is_above_water = False
+    for j in range(N):
+        if H[j] > i:
+            if not is_above_water:
+                cnt += 1
+                is_above_water = True
+        else:
+            is_above_water = False
+    
+    max_cnt = max(max_cnt, cnt)
+
+print(max_cnt)
