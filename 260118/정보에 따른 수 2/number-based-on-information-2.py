b5bb716b084db8a33b4b6eaf1c6342254dfb6f5b
codejwj <jsin1336@gmail.com>
2026-01-18 21:04:57 +0900

    Title: 정보에 따른 수 2, Time: 70ms, Memory: 17MB, Status: Passed - Codetree

--- 260118/정보에 따른 수 2/number-based-on-information-2.py
+++ 260118/정보에 따른 수 2/number-based-on-information-2.py
@@ -1,21 +1,24 @@
-MAX_NUM = 1000
+import sys
+
+INT_MAX = sys.maxsize
 
 T, a, b = tuple(map(int, input().split()))
-P = []
-
-for _ in range(T):
-    char, pos = tuple(input().split())
-    P.append((char, int(pos)))
+sn_data = [
+    tuple(input().split())
+    for _ in range(T)
+]
 
 ans = 0
 for k in range(a, b + 1):
-    d1 = MAX_NUM
-    d2 = MAX_NUM
-    for i in range(T):
-        if P[i][0] == 'S':
-            d1 = min(d1, abs(k - P[i][1]))
-        elif P[i][0] == 'N':
-            d2 = min(d2, abs(k - P[i][1]))
+    d1 = INT_MAX
+    d2 = INT_MAX
+    for char, pos in sn_data:
+        pos = int(pos)
+
+        if char == 'S':
+            d1 = min(d1, abs(k - pos))
+        elif char == 'N':
+            d2 = min(d2, abs(k - pos))
             
     if d1 <= d2:
         ans += 1
