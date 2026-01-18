b1e56d255b6e4b355cfd2c6918543c3f89511509
codejwj <jsin1336@gmail.com>
2026-01-18 21:44:42 +0900

    Title: 야바위, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260118/야바위/ya-rock.py
+++ 260118/야바위/ya-rock.py
@@ -1,21 +1,23 @@
 N = int(input())
-moves = [
+commands = [
     tuple(map(int, input().split()))
     for _ in range(N)
 ]
 
 max_score = 0
-for pos in [1, 2, 3]:
-    current_pos = pos
-    score = 0
+#시작 위치를 전부 가정
+for i in range(1, 4):
+    yabawi = [0] * 4
+    #i번째 종이컵에 처음 조약돌을 넣고 시작
+    yabawi[i] = 1
 
-    for a, b, c in moves:
-        if current_pos == a:
-            current_pos = b
-        elif current_pos == b:
-            current_pos = a
-        
-        if current_pos == c:
+    score = 0
+    for a, b, c in commands:
+        #두 종이컵을 교환
+        yabawi[a], yabawi[b] = yabawi[b], yabawi[a]
+
+        #교환 이후 c번에 돌이 있다면 점수를 얻음
+        if yabawi[c]:
             score += 1
     
     max_score = max(max_score, score)
