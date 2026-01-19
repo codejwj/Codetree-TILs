4af39b4518c05ba992aa77b6251df96d1ab2f385
codejwj <jsin1336@gmail.com>
2026-01-19 12:33:19 +0900

    Title: 좌표평면 위의 균형 2, Time: 74ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260119/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,32 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+N = int(input())
+points = [
+    tuple(map(int, input().split())) 
+    for _ in range(N)
+]
+
+x_list = list(set([p[0] + 1 for p in points]))
+y_list = list(set([p[1] + 1 for p in points]))
+
+ans = INT_MAX
+for x_line in x_list:
+    for y_line in y_list:
+        q1, q2, q3, q4 = 0, 0, 0, 0
+        M_cnt = 0
+        for px, py in points:
+            if px > x_line and py > y_line:
+                q1 += 1
+            elif px < x_line and py > y_line:
+                q2 += 1
+            elif px < x_line and py < y_line:
+                q3 += 1
+            elif px > x_line and py < y_line:
+                q4 += 1
+            
+        M_cnt = max(q1, q2, q3, q4)  
+        ans = min(ans, M_cnt)
+
+print(ans)
