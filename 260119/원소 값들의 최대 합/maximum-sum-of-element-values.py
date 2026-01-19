e3c78c62b37e6dd3ef1a76581c37c55540c5cf16
codejwj <jsin1336@gmail.com>
2026-01-19 12:13:22 +0900

    Title: 원소 값들의 최대 합, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260119/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 260119/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -3,13 +3,15 @@ arr = [0] + list(map(int, input().split()))
 
 max_sum = 0
 for i in range(1, N + 1):
-    current_pos = i
-    sum_val = 0
-    for j in range(1, M + 1):
-        elem = arr[current_pos]        
-        sum_val += elem
-        current_pos = elem
+    #시작점은 i
+    cur = i
+    sum_element = 0
+
+    #M번 움직임을 반복
+    for _ in range(M):
+        sum_element += arr[cur]
+        cur = arr[cur]
     
-    max_sum = max(max_sum, sum_val)
+    max_sum = max(max_sum, sum_element)
 
 print(max_sum)
