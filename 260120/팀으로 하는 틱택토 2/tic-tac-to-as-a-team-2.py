769cb3bd97bca71b3807ebe8f7022cb3f48f02f2
codejwj <jsin1336@gmail.com>
2026-01-20 23:30:20 +0900

    Title: 팀으로 하는 틱택토 2, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260120/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 260120/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,15 +1,17 @@
 inp = [input() for _ in range(3)]
 
-winning_teams = set()
-for i in range(3):
-    if len(set(inp[i])) == 2:
-        winning_teams.add(tuple(sorted(list(set(inp[i])))))
-    if len(set(inp[0][i] + inp[1][i] + inp[2][i])) == 2:
-        winning_teams.add(tuple(sorted(list(set(inp[0][i] + inp[1][i] + inp[2][i])))))
+all_lines = []
+all_lines.extend(inp) #가로선 추가
+all_lines.extend(zip(*inp)) #세로선 추가
+all_lines.append(inp[0][0] + inp[1][1] + inp[2][2]) #대각선1 추가
+all_lines.append(inp[0][2] + inp[1][1] + inp[2][0]) #대각선2 추가
 
-if len(set(inp[0][0] + inp[1][1] + inp[2][2])) == 2:
-    winning_teams.add(tuple(sorted(list(set(inp[0][0] + inp[1][1] + inp[2][2])))))
-if len(set(inp[0][2] + inp[1][1] + inp[2][0])) == 2:
-    winning_teams.add(tuple(sorted(list(set(inp[0][2] + inp[1][1] + inp[2][0])))))
-        
+#팀 목록을 저장
+winning_teams = set()
+for line in all_lines:
+    s = set(line)
+    if len(s) == 2:
+        #팀원 2명을 정렬해서 튜플로 만든 뒤 추가(중복 방지)
+        winning_teams.add(tuple(sorted(s)))
+
 print(len(winning_teams))
