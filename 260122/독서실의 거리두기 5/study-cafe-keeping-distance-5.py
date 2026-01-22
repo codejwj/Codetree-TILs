faed00c609c12b97217e4f12fbce072dab30518a
codejwj <jsin1336@gmail.com>
2026-01-22 11:13:46 +0900

    Title: 독서실의 거리두기 5, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 260122/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,26 +1,28 @@
 N = int(input())
 seat = list(input())
 
-def min_dist(x):
-    arr = []
+def min_dist():
+    dist = N
+    #둘 다 1인 곳에 대해
+    #모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구함
     for i in range(N):
-        if x[i] == '1':
-            arr.append(i)
-    
-    min_val = N
-    for j in range(len(arr) - 1):
-        val = abs(arr[j] - arr[j + 1])
-        min_val = min(min_val, val)
+        for j in range(i + 1, N):
+            if seat[i] == '1' and seat[j] == '1':
+                dist = min(dist, j - i)
 
-    return min_val
+    return dist
     
 ans = 0
+#들어갈 위치를 일일히 정해보며
+#그 상황에서 가장 가까운 사람의 거리를 구해
+#가능한 경우 중 최댓값을 계산
 for i in range(N):
     if seat[i] == '0':
+        #비어 있는 위치에 인원을 배치
         seat[i] = '1'
-        dist = min_dist(seat)
+        #가장 가까운 사람간의 거리를 구해 최댓값을 갱신
+        ans = max(ans, min_dist())
+        #다시 채워졌던 값을 되돌림
         seat[i] = '0'
-        
-        ans = max(ans, dist)
 
 print(ans)
