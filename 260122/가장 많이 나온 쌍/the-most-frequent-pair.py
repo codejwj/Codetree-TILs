9962e1e49fc865125d26523c1aa5b94466aa6304
codejwj <jsin1336@gmail.com>
2026-01-22 12:12:42 +0900

    Title: 가장 많이 나온 쌍, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 260122/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -4,11 +4,9 @@ pairs = [
     for _ in range(M)
 ]
 
-pairs.sort()
-
 max_cnt = 0
 for i in range(1, N + 1):
-    for j in range(1, N + 1):
+    for j in range(i + 1, N + 1):
         cnt = 0
         for a, b in pairs:
             if (a == i and b == j) or (a == j and b == i):
