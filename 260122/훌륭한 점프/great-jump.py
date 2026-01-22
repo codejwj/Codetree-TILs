32e200c0604e5bd5a4e5fe80a71c655a1c84c34d
codejwj <jsin1336@gmail.com>
2026-01-22 13:14:32 +0900

    Title: 훌륭한 점프, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/훌륭한 점프/great-jump.py
+++ 260122/훌륭한 점프/great-jump.py
@@ -3,23 +3,22 @@ MAX_NUM = 100
 N, K = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-def is_possible(max_val):
-    indices = []
-    for i, elem in enumerate(arr):
-        if elem <= max_val:
-            indices.append(i)
-    
-    arr_size = len(indices)
-    for i in range(1, arr_size):
-        dist = indices[i] - indices[i - 1]
-        if dist > K:
-            return False
+def is_possible(limit):
+    #마지막 index로부터 숫자 limit을 넘지 않으면서
+    #거리 K이내로 계속 이동이 가능한지를 판단
+    last_idx = 0
+    for i in range(1, N):
+        if arr[i] <= limit:
+            if i - last_idx > K:
+                return False
+            last_idx = i
     
     return True
 
-minimax = MAX_NUM
-for a in range(max(arr[0], arr[-1]), max(arr) + 1):
-    if is_possible(a):
-        minimax = min(minimax, a)
-    
-print(minimax)
+#밟으며 지나간 최댓값을 i라고 가정
+#거리 K이내로 점프하며 끝까지 도달하는 것이 가능한지를 살펴봄
+#가능하다면, 그때의 i가 최솟값이므로 답을 출력하고 종료
+for i in range(max(arr[0], arr[N - 1]), MAX_NUM + 1):
+    if is_possible(i):
+        print(i)
+        break
