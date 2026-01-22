2683c74224580310a65b250a178a630e3f474c77
codejwj <jsin1336@gmail.com>
2026-01-22 13:05:42 +0900

    Title: 훌륭한 점프, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260122/훌륭한 점프/great-jump.py
@@ -0,0 +1,25 @@
+MAX_NUM = 100
+
+N, K = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+def is_possible(max_val):
+    indices = []
+    for i, elem in enumerate(arr):
+        if elem <= max_val:
+            indices.append(i)
+    
+    arr_size = len(indices)
+    for i in range(1, arr_size):
+        dist = indices[i] - indices[i - 1]
+        if dist > K:
+            return False
+    
+    return True
+
+minimax = MAX_NUM
+for a in range(max(arr[0], arr[-1]), max(arr)):
+    if is_possible(a):
+        minimax = min(minimax, a)
+    
+print(minimax)
