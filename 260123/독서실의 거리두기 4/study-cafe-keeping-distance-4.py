f468211cea5ee7882492e9fa9d243b94f5559169
codejwj <jsin1336@gmail.com>
2026-01-23 15:23:07 +0900

    Title: 독서실의 거리두기 4, Time: 72ms, Memory: 17MB, Status: Passed - Codetree

--- 260123/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 260123/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -5,17 +5,17 @@ def min_dist():
     dist = N
     for i in range(N):
         for j in range(i + 1, N):
-            for k in range(j + 1, N):
-                if seat[i] == '1' and seat[j] == '1' and seat[k] == '1':
-                    dist = min(dist, k - j, j - i)
+            if seat[i] == '1' and seat[j] == '1':
+                dist = min(dist, j - i)
     
     return dist
 
 ans = 0
 for i in range(N):
-    if seat[i] == '0':
-        seat[i] = '1'
-        ans = max(ans, min_dist())
-        seat[i] = '0'
+    for j in range(i + 1, N):
+        if seat[i] == '0' and seat[j] == '0':
+            seat[i], seat[j] = '1', '1'
+            ans = max(ans, min_dist())
+            seat[i], seat[j] = '0', '0'
 
 print(ans)
