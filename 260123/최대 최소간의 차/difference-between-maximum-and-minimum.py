d2d6f25fa5d0de18c121b85dd27e130ad702ccd5
codejwj <jsin1336@gmail.com>
2026-01-23 17:32:04 +0900

    Title: 최대 최소간의 차, Time: 66ms, Memory: 17MB, Status: Passed - Codetree

--- 260123/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 260123/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -1,22 +1,23 @@
 import sys
 
 INT_MAX = sys.maxsize
-MAX_NUM = 10000
+MAX_K = 10000
 
 N, K = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-min_cost = INT_MAX
-for a in range(1, MAX_NUM + 1):
+def get_cost(low, high):
     cost = 0
     for elem in arr:
-        if a <= elem and elem <= a + K:
-            continue
-        elif elem < a:
-            cost += (a - elem)
-        else: #elem > a + K
-            cost += elem - (a + K)
-    
-    min_cost = min(min_cost, cost)
+        if elem < low:
+            cost += low - elem
+        elif elem > high:
+            cost += elem - high
+
+    return cost
+
+min_cost = INT_MAX
+for num in range(1, MAX_K + 1):
+    min_cost = min(min_cost, get_cost(num, num + K))
 
 print(min_cost)
