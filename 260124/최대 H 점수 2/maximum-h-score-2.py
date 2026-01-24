f1e7770200e018c026146403e549eb2274ee4d9e
codejwj <jsin1336@gmail.com>
2026-01-24 23:21:02 +0900

    Title: 최대 H 점수 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260124/최대 H 점수 2/maximum-h-score-2.py
+++ 260124/최대 H 점수 2/maximum-h-score-2.py
@@ -1,22 +1,25 @@
-MAX_NUM = 100
-
 N, L = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
 ans = 0
-for num in range(1, N + 1):
-    cnt_H = 0
+for h in range(1, N + 1):
+    #정답이 h일 때 가능한 지 판단
+
+    #h - 1인 값은 최대 L개까지 h로 올릴 수 있음
+    #cnt_h : h 이상인 숫자의 개수(h - 1인 숫자는 L개까지 카운트)
+    #cnt_L : 지금까지 1 증가시킨 숫자의 개수
+    cnt_h = 0
     cnt_L = 0
     for i in range(N):
-        if arr[i] >= num:
-            cnt_H += 1
-        elif arr[i] == num - 1:
+        if arr[i] >= h:
+            cnt_h += 1
+        elif arr[i] == h - 1:
             if cnt_L < L:
                 cnt_L += 1
-                cnt_H += 1
+                cnt_h += 1
         
-        if cnt_H >= num:
-            ans = num
+        if cnt_h >= h:
+            ans = h
             break
-
+            
 print(ans)
