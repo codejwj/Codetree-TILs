d6dc087ad5ec849f8dcee1a4a64b7a65c2ea849d
codejwj <jsin1336@gmail.com>
2026-01-25 15:43:32 +0900

    Title: 구간 잘 나누기, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 260125/구간 잘 나누기/divide-sections-well.py
+++ 260125/구간 잘 나누기/divide-sections-well.py
@@ -1,36 +1,34 @@
-import sys
-
-INT_MAX = sys.maxsize
+MAX_A = 10000
 
 N, M = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-def is_solve(limit):
-    if limit < max(arr):
-        return False
-
-    cnt = 1
-    total = 0
-    for num in arr:
-        if total + num > limit:
-            cnt += 1
-            total = num
-        else:
-            total += num
+ans = MAX_A
+#구간의 합의 최댓값이 a일 때
+for a in range(1, MAX_A + 1):
+    #possible : 구간을 나눌 수 있으면 True
+    #section : 나뉘어져야 하는 구간의 개수
+    possible = True
+    section = 1
     
-    return cnt <= M
+    cnt = 0
+    for i in range(N):
+        #숫자 하나가 a보다 크면 구간을 나눌 수 없음
+        if arr[i] > a:
+            possible = False
+            break
 
-low = max(arr)
-high = sum(arr)
-
-ans = INT_MAX
-while low <= high:
-    mid = (low + high) // 2
+        #i번째 숫자가 들어갔을 때 a보다 커지면
+        #i번째 숫자부터 다음 구간으로 만듬
+        if cnt + arr[i] > a:
+            cnt = 0
+            section += 1
+        
+        #이번 구간에 i번째 숫자를 집어넣음
+        cnt += arr[i]
     
-    if is_solve(mid):
-        ans = mid
-        high = mid - 1
-    else:
-        low = mid + 1
+    if possible and section <= M:
+        ans = min(ans, a)
+        break
 
 print(ans)
