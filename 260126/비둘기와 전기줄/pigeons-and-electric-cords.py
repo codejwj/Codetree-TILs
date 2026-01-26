543549e9e9825c14d3f102ff1b5440fdf4af7f15
codejwj <jsin1336@gmail.com>
2026-01-26 18:56:37 +0900

    Title: 비둘기와 전기줄, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260126/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 260126/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -1,20 +1,20 @@
 N = int(input())
-arr = []
+movements = [
+    tuple(map(int, input().split()))
+    for _ in range(N)
+]
 
-for _ in range(N):
-    p, pos = tuple(map(int, input().split()))
-    arr.append((p, pos))
+#현재 비둘기의 위치를 기록
+#-1이면 아직 미정인 상태
+pos = [-1] * 11
 
-state = [-1] * 11
+move_cnt = 0
+for pigeon, move_dir in movements:
+    #해당 비둘기의 위치가 미정이 아니고 옮겨간 위치와 다른 위치라면 
+    #이동 횟수 증가
+    if pos[pigeon] != -1 and pos[pigeon] != move_dir:
+        move_cnt += 1
 
-cnt = 0
-for i in range(N):
-    p = arr[i][0]
-    pos = arr[i][1]
+    pos[pigeon] = move_dir
 
-    if state[p] != -1 and state[p] != pos:
-        cnt += 1
-        
-    state[p] = pos
-
-print(cnt)
+print(move_cnt)
