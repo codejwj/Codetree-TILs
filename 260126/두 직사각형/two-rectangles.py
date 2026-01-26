aa8d55c89cf8ae44f4a0dcd6e9c57e964f08560f
codejwj <jsin1336@gmail.com>
2026-01-26 15:16:32 +0900

    Title: 두 직사각형, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260126/두 직사각형/two-rectangles.py
+++ 260126/두 직사각형/two-rectangles.py
@@ -1,13 +1,19 @@
 x1, y1, x2, y2 = tuple(map(int, input().split()))
 a1, b1, a2, b2 = tuple(map(int, input().split()))
 
-def overlapping(x1, x2, a1, a2):
+def overlapping1(x1, x2, a1, a2):
     if (x2 < a1) or (a2 < x1):
         return False
     else:
         return True
 
-if overlapping(x1, x2, a1, a2):
+def overlapping2(y1, y2, b1, b2):
+    if (b2 < y1) or (y2 < b1):
+        return False
+    else:
+        return True
+
+if overlapping1(x1, x2, a1, a2) and overlapping2(y1, y2, b1, b2):
     print("overlapping")
 else:
     print("nonoverlapping")
