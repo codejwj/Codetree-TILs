5b3ce6dc9f4c39d4365f5eb7f6e423d68ed1a0f9
codejwj <jsin1336@gmail.com>
2026-01-26 16:01:48 +0900

    Title: 구역 청소, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260126/구역 청소/district-cleaning.py
+++ 260126/구역 청소/district-cleaning.py
@@ -1,10 +1,10 @@
 a, b = tuple(map(int, input().split()))
 c, d = tuple(map(int, input().split()))
 
-def is_area(a, b, c, d):
-    if (b < c) or (d < a):
-        return (b - a) + (d - c)
+def intersecting(x1, x2, x3, x4):
+    if (x2 < x3) or (x4 < x1):
+        return (x2 - x1) + (x4 - x3)
     else:
-        return max(b, d) - min(a, c)
+        return max(x2, x4) - min(x1, x3)
 
-print(is_area(a, b, c, d))
+print(intersecting(a, b, c, d))
