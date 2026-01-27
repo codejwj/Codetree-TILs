8c2f21a3ab76e5f3f6db08c116dc71bab34bd5da
codejwj <jsin1336@gmail.com>
2026-01-27 09:32:33 +0900

    Title: 달리자, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260127/달리자/run-and-run.py
+++ 260127/달리자/run-and-run.py
@@ -2,10 +2,16 @@ N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-total_dist = 0
-for i in range(N - 1):
-    res = A[i] - B[i]
-    total_dist += res
-    A[i + 1] += res
+ans = 0
 
-print(total_dist)
+#마지막 집은 사람을 보낼 곳이 없으므로 N - 1까지 설정
+for i in range(N - 1):
+    if A[i] > B[i]:
+        num = A[i] - B[i]
+        #현재 집 인원을 B[i]로 맞춤
+        A[i] -= num
+        #다음 집으로 인원 이동
+        A[i + 1] += num
+        ans += num
+
+print(ans)
