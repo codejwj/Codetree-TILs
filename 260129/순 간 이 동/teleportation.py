0367706b5788499ec32f1bbbe702273ae8deebe7
codejwj <jsin1336@gmail.com>
2026-01-29 11:57:23 +0900

    Title: 순 간 이 동, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260129/순 간 이 동/teleportation.py
+++ 260129/순 간 이 동/teleportation.py
@@ -1,7 +1,12 @@
 A, B, x, y = tuple(map(int, input().split()))
 
+#case 1. a -> b 바로 이동
 dist1 = abs(A - B)
+
+#case 2. a -> x -> y -> b 순서로 이동
 dist2 = abs(A - x) + abs(B - y)
+
+#case 3. a -> y -> x -> b 순서로 이동
 dist3 = abs(A - y) + abs(B - x)
 
 print(min(dist1, dist2, dist3))
