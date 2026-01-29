b8a5b4e2d9aabcd76efbc086d084a185bc69d941
codejwj <jsin1336@gmail.com>
2026-01-29 13:30:28 +0900

    Title: 독서실의 거리두기 3, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260129/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 260129/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -7,22 +7,18 @@ for i in range(N):
     if seats[i] == '1':
         arr.append(i)
 
-arr.sort()
-
 max_dist = 0
 for i in range(len(arr) - 1):
-    max_dist = max(max_dist, arr[i + 1] - arr[i])
+    dist = arr[i + 1] - arr[i]
+    if max_dist <= dist:
+        max_dist = dist
+        mid = (arr[i + 1] + arr[i]) // 2
 
-    mid = (arr[i + 1] + arr[i]) // 2
-
-    if seats[mid] == '0':
-        seats[mid] = '1'
-        arr.append(mid)
+if seats[mid] == '0':
+    seats[mid] = '1'
+    arr.append(mid)
 
 arr.sort()
 
-ans = 0
-for i in range(len(arr) - 1):
-    ans = max(ans, arr[i + 1] - arr[i])
-
+ans = max_dist // 2
 print(ans)
