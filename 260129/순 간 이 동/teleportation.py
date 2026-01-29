d92ef853e72f8fe9f946a651d7a2c44c8c3d3e68
codejwj <jsin1336@gmail.com>
2026-01-29 11:53:09 +0900

    Title: 순 간 이 동, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 260129/순 간 이 동/teleportation.py
+++ 260129/순 간 이 동/teleportation.py
@@ -1,8 +1,7 @@
 A, B, x, y = tuple(map(int, input().split()))
 
-if abs(A - B) < abs(x - y):
-    print(abs(A - B))
-elif x < y:
-    print(abs(A - x) + abs(B - y))
-else:
-    print(abs(A - y) + abs(B - x))
+dist1 = abs(A - B)
+dist2 = abs(A - x) + abs(B - y)
+dist3 = abs(A - y) + abs(B - x)
+
+print(min(dist1, dist2, dist3))
