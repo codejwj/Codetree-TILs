9b5107f587ec5cca3376702d9f7a05a90bc9ee8d
codejwj <jsin1336@gmail.com>
2026-01-29 12:16:42 +0900

    Title: 연속된 숫자 만들기 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260129/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -0,0 +1,10 @@
+pos = list(map(int, input().split()))
+
+pos.sort()
+
+if pos[1] - pos[0] == 1 and pos[2] - pos[1] == 1:
+    print(0)
+elif pos[1] - pos[0] == 2 or pos[2] - pos[1] == 2:
+    print(1)
+else:
+    print(2)
