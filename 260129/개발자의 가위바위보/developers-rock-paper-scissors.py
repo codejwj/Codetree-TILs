cbb13cf5c5e46e1f7fa188f3b941c3d872192b8b
codejwj <jsin1336@gmail.com>
2026-01-29 17:06:14 +0900

    Title: 개발자의 가위바위보, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260129/개발자의 가위바위보/developers-rock-paper-scissors.py
+++ 260129/개발자의 가위바위보/developers-rock-paper-scissors.py
@@ -7,12 +7,20 @@ moves = [
 cnt1 = 0
 cnt2 = 0
 for i in range(N):
-    if 1 < 2 or 2 < 3 or 3 < 1:
-        if moves[i][0] > moves[i][1]:
+    if moves[i][0] == 1:
+        if moves[i][1] == 3:
             cnt1 += 1
-        elif moves[i][0] < moves[i][1]:
+        elif moves[i][1] == 2:
             cnt2 += 1
-        else:
-            pass
-
+    elif moves[i][0] == 2:
+        if moves[i][1] == 1:
+            cnt1 += 1
+        elif moves[i][1] == 3:
+            cnt2 += 1
+    elif moves[i][0] == 3:
+        if moves[i][1] == 2:
+            cnt1 += 1
+        elif moves[i][1] == 1:
+            cnt2 += 1
+        
 print(max(cnt1, cnt2))
