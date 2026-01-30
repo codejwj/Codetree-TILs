dccecf98cce522681793f381479f82fd67e73f23
codejwj <jsin1336@gmail.com>
2026-01-30 19:19:02 +0900

    Title: ABC 줄 세우기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260130/ABC 줄 세우기/abc-line-up.py
+++ 260130/ABC 줄 세우기/abc-line-up.py
@@ -3,9 +3,14 @@ arr = list(input().split())
 
 cnt = 0
 for i in range(N):
-    for j in range(N - 1 - i):
-        if arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
-            cnt += 1
-             
+    #이번 차례에 올 알파벳 결정
+    x = chr(ord('A') + i)
+
+    #현재 배열에서 x의 위치를 찾기
+    idx = arr.index(x)
+    #찾은 위치 idx부터 목표 위치 i까지 역순으로 인접 교환 
+    for j in range(idx, i, -1):
+        arr[j], arr[j - 1] = arr[j - 1], arr[j]
+        cnt += 1
+
 print(cnt)
