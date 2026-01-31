20713720a1003db753768571260f667885e1dc97
codejwj <jsin1336@gmail.com>
2026-01-31 15:52:20 +0900

    Title: 독서실의 거리두기 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260131/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,40 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+N = int(input())
+seats = list(input())
+
+arr = []
+
+for i in range(N):
+    if seats[i] == '1':
+        arr.append(i)
+
+left_gap = arr[0]
+right_gap = (N - 1) - arr[-1]
+
+if left_gap >= right_gap:
+    max_dist = left_gap
+    pos = 0
+else:
+    max_dist = right_gap
+    pos = N - 1
+
+for i in range(len(arr) - 1):
+    dist = (arr[i + 1] - arr[i]) // 2 
+    if max_dist < dist:
+        max_dist = dist
+        pos = (arr[i + 1] + arr[i]) // 2
+    
+if seats[pos] == '0':
+    seats[pos] = '1'
+    arr.append(pos)
+
+arr.sort()
+
+ans = INT_MAX
+for i in range(len(arr) - 1):
+    ans = min(ans, arr[i + 1] - arr[i])
+
+print(ans)
