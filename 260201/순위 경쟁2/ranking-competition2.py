7ef45aae44233dee08ced1fee4fcd9b78031a026
codejwj <jsin1336@gmail.com>
2026-02-01 20:50:31 +0900

    Title: 순위 경쟁2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260201/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,35 @@
+N = int(input())
+arr = []
+
+for _ in range(N):
+    c, s = input().split()
+    s = int(s)
+    arr.append((c, s))
+
+cnt = 0
+leader = 0
+score_A = 0
+score_B = 0
+for c, s in arr:
+    if c == 'A':
+        score_A += s
+    elif c == 'B':
+        score_B += s
+
+    if score_A > score_B:
+        if leader != 1:
+            cnt += 1
+        
+        leader = 1
+    elif score_A < score_B:
+        if leader != 2:
+            cnt += 1
+        
+        leader = 2
+    else:
+        if leader != 3:
+            cnt += 1
+        
+        leader = 3
+
+print(cnt)
