ec1d806f5af802ca39797a8eae98546dbbbf6fdd
codejwj <jsin1336@gmail.com>
2026-02-02 13:49:47 +0900

    Title: 순위 경쟁, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260202/순위 경쟁/ranking-competition.py
+++ 260202/순위 경쟁/ranking-competition.py
@@ -8,11 +8,15 @@ for _ in range(N):
 
 score_A = score_B = score_C = 0
 
+#명예의 전당 상태를 반환 
 def get_status(score1, score2, score3):
     max_score = max(score1, score2, score3)
-        
+    
+    #각 점수가 최고점과 같은 지 확인(True / False 묶음 반환)
     return (score1 == max_score, score2 == max_score, score3 == max_score)
 
+#처음에는 A, B, C가 모두 0점이기 때문에
+#A, B, C 모두 명예의 전당에 올라가 있음
 mvp = (True, True, True)
 cnt = 0
 
