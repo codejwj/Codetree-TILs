c2ae5efe56935b66d77ff2b6848f9d1b28b42851
codejwj <jsin1336@gmail.com>
2026-02-02 12:30:14 +0900

    Title: 두 번째로 작은 수의 위치, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260202/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 260202/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -1,23 +1,12 @@
-MAX_NUM = 100
-
-N = int(input())
+n = int(input())
 a = list(map(int, input().split()))
 
-ans = -1
-min1 = MAX_NUM
-min2 = MAX_NUM
-idx1 = 0
-idx2 = 0
+pos = list(set(a))
+pos.sort()
 
-for i in range(N):
-    if a[i] < min1:
-        min2 = min1
-        idx2 = idx1
-        min1 = a[i]
-        idx1 = i + 1
-    elif min1 < a[i] < min2:
-        min2 = a[i]
-        idx2 = i + 1
-        ans = idx2
-
-print(ans)
+if len(pos) < 2:
+    print(-1)
+elif a.count(pos[1]) >= 2:
+    print(-1)
+else:
+    print(a.index(pos[1]) + 1)
