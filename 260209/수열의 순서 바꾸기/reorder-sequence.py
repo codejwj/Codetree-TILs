2f88d842488b8be12d01a288898d13565791dddb
codejwj <jsin1336@gmail.com>
2026-02-09 13:40:01 +0900

    Title: 수열의 순서 바꾸기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260209/수열의 순서 바꾸기/reorder-sequence.py
+++ 260209/수열의 순서 바꾸기/reorder-sequence.py
@@ -2,14 +2,11 @@ N = int(input())
 arr = list(map(int, input().split()))
 
 cnt = 0
-for i in range(N):
-    satisfied = False
-    for j in range(i, N - 1):
-        if arr[j] > arr[j + 1]:
-            satisfied = True
-            break
-
-    if satisfied:
-        cnt += 1
+for i in range(N - 2, -1, -1):
+    #뒤에서부터 탐색하며 정렬이 깨지는 마지막 위치를 찾아
+    #그 앞의 모든 원소를 이동 횟수로 계산
+    if arr[i] > arr[i + 1]:
+        cnt = i + 1
+        break
 
 print(cnt)
