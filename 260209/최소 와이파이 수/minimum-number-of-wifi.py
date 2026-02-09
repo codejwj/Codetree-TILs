15afd25f59cc37c766673fff1eda45a20192a938
codejwj <jsin1336@gmail.com>
2026-02-09 12:46:51 +0900

    Title: 최소 와이파이 수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260209/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,17 @@
+N, M = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+wifi_cnt = 0
+pos = 0
+
+while pos < N:
+    if arr[pos] == 1:
+        #사람이 있다면 wifi를 설치
+        #현재 위치(i)부터 (i + 2 * M)까지 사용 가능
+        wifi_cnt += 1
+        pos = pos + (2 * M) + 1
+    else:
+        #사람이 없다면 다음 칸으로 이동
+        pos += 1
+
+print(wifi_cnt)    
