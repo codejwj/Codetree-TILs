b7304ae5a9265a41ca0f8bec435ca739ad9e37f7
codejwj <jsin1336@gmail.com>
2026-02-09 13:08:21 +0900

    Title: 2개씩 그룹짓기 2, Time: 115ms, Memory: 35MB, Status: Passed - Codetree

--- 260209/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 260209/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -8,8 +8,10 @@ arr = list(map(int, input().split()))
 arr.sort()
 
 ans = INT_MAX
+#정렬한 뒤 i번째 값과 N + i번째 값의 차이를 구해
+#구한 값들 중에서 최솟값을 찾음
 for i in range(N):
-    diff = abs(arr[i] - arr[N + i])
+    diff = arr[N + i] - arr[i]
     ans = min(ans, diff)
 
 print(ans)
