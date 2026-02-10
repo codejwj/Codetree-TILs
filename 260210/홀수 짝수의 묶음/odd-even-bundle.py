379b3dd1ecfda79c52f8fdb9d808c0ffa6b30864
codejwj <jsin1336@gmail.com>
2026-02-10 15:01:41 +0900

    Title: 홀수 짝수의 묶음, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260210/홀수 짝수의 묶음/odd-even-bundle.py
+++ 260210/홀수 짝수의 묶음/odd-even-bundle.py
@@ -1,37 +1,43 @@
 N = int(input())
 arr = list(map(int, input().split()))
 
-even_cnt = 0
-odd_cnt = 0
+even= 0
+odd = 0
 for elem in arr:
     if elem % 2 == 0:
-        even_cnt += 1
-    elif elem % 2 != 0:
-        odd_cnt += 1
+        even += 1
+    else:
+        odd += 1
 
-ans = 0
-target_is_even = True
-
+#만들어진 그룹의 총 개수
+groups = 0
 while True:
-    if target_is_even == True:
-        if even_cnt > 0:
-            even_cnt -= 1
-            ans += 1
-        elif even_cnt == 0 and odd_cnt >= 2:
-            odd_cnt -= 2
-            ans += 1
+    #짝수 번째 그룹은 합이 '짝수'여야 함 
+    if groups % 2 == 0:
+        #짝수가 남아있다면 짝수 1개 사용 
+        if even:
+            even -= 1
+            groups += 1
+        #짝수가 없고 홀수가 2개 이상 있다면 홀수 2개 사용
+        elif odd >= 2:
+            odd -= 2
+            groups += 1
+        #짝수 그룹을 만들 수 없는 경우
+        #남은 숫자가 있다면 마지막 그룹에 합쳐야 하므로
+        #그룹 수 하나 감소
         else:
+            if even > 0 or odd > 0:
+                groups -= 1
             break
-    elif target_is_even == False:
-        if odd_cnt > 0:
-            odd_cnt -= 1
-            ans += 1
+
+    #홀수 번째 그룹은 합이 '홀수'여야 함
+    else:
+        #홀수 숫자가 남아있다면 홀수 1개 사용 
+        if odd:
+            odd -= 1
+            groups += 1
+        #홀수가 하나도 없다면 더 이상 홀수 그룹 생성 불가
         else:
             break
     
-    target_is_even = not target_is_even
-
-if odd_cnt == 1:
-    ans -= 1
-
-print(max(0, ans))
+print(groups)
