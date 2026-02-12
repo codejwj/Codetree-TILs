70b70f973c4319c57ef1e9057ac4df50d560aff6
codejwj <jsin1336@gmail.com>
2026-02-12 11:51:48 +0900

    Title: A, B, C 찾기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260212/A, B, C 찾기/finding-a-b-c.py
+++ 260212/A, B, C 찾기/finding-a-b-c.py
@@ -1,14 +1,8 @@
-import sys
-
-MAX_NUM = 100
-
 arr = list(map(int, input().split()))
 arr.sort()
 
-for A in range(1, MAX_NUM):
-    for B in range(A, MAX_NUM):
-        for C in range(B, MAX_NUM):
-            list = [A, B, C, A + B, B + C, C + A, A + B + C]
-            if sorted(list) == arr:
-                print(A, B, C)
-                sys.exit()
+A = arr[0]
+B = arr[1]
+C = arr[-1] - A - B
+
+print(A, B, C)
