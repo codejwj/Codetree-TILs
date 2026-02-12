f2713a4718ad7242fe8696f10da95d26558c1665
codejwj <jsin1336@gmail.com>
2026-02-12 11:54:36 +0900

    Title: A, B, C 찾기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260212/A, B, C 찾기/finding-a-b-c.py
+++ 260212/A, B, C 찾기/finding-a-b-c.py
@@ -1,6 +1,10 @@
 arr = list(map(int, input().split()))
+#오름차순으로 정렬
 arr.sort()
 
+#가장 작은 숫자는 A, 두 번째로 작은 숫자는 항상 B
+#가장 큰 숫자는 A + B + C 
+#C는 끝 숫자 - A - B로 계산
 A = arr[0]
 B = arr[1]
 C = arr[-1] - A - B
